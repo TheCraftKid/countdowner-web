ca7670b43ff8f432467c9400d04ce06a37122fdb
Willie Chalmers III <willieechalmers@gmail.com>
2018-12-02 21:24:55 -0700

    chore: Update dependencies
    Add Material progress spinner and fullscreen helper library

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import Fullscreen from "vue-fullscreen";
 import {
   MdButton,
   MdContent,
@@ -12,9 +13,12 @@ import {
   MdCard,
   MdIcon,
   MdList,
+  MdProgress,
 } from "vue-material/dist/components";
 import "vue-material/dist/vue-material.min.css";
+import "@material/typography/dist/mdc.typography.css";
 
+Vue.use(Fullscreen, { name: "vue-fullscreen" });
 Vue.use(MdButton);
 Vue.use(MdContent);
 Vue.use(MdTabs);
@@ -24,6 +28,7 @@ Vue.use(MdToolbar);
 Vue.use(MdCard);
 Vue.use(MdIcon);
 Vue.use(MdList);
+Vue.use(MdProgress);
 import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
